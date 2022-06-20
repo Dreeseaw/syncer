c3ac6927e9e1ba3649698c357e08bcf50197700a
William Dreese <wdreese123@gmail.com>
2022-06-20 10:55:02 -0400

    First (#1)
    * ci, simple syncer
    
    * go pkg; tests pass

---
+++ syncer.go
@@ -0,0 +1,51 @@
+package syncer
+
+import (
+    "fmt"
+    "errors"
+
+    "github.com/kelindar/column"
+    "github.com/kelindar/column/commit"
+)
+
+type Syncer struct {
+    stream chan commit.Commit
+    target *column.Collection 
+}
+
+func NewSyncer() *Syncer {
+    return &Syncer{
+        stream: make(chan commit.Commit),
+        target: nil,
+    }
+}
+
+// Append fulfills commit.Logger
+func (s *Syncer) Append(comm commit.Commit) error {
+    s.stream <- comm.Clone()
+    return nil
+}
+
+func (s *Syncer) Assign(coll *column.Collection) error {
+    if s.target != nil {
+        return errors.New("syncer already assigned")
+    }
+    s.target = coll
+    return nil
+}
+
+func (s *Syncer) Start() {
+    
+    // start grpc client
+
+    // start grpc server
+
+    go func(){
+        for change := range s.stream {
+            // send change to other nodes
+            fmt.Println(change)
+        }
+        return
+    }()
+
+}
